360b0482ad6800dda157505b31a1827192e56148
AnjanaSKamath <98830829+AnjanaSKamath@users.noreply.github.com>
2023-09-13 14:45:15 +0530

    initial

---
+++ eventapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Applicant
+
+class Applicantform(forms.ModelForm):
+    class Meta:
+        model = Applicant
+        fields = ['full_name','email','phone']
+        
+
+
